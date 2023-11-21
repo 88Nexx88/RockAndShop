a81bab744cc20de0c2b9dd8b871bd1a14b465d31
valery <ValerOkun@yandex.ru>
2023-11-21 12:33:17 +0300

    Ready DEMO DB and find products

--- code/final.py
+++ code/final.py
@@ -1,51 +1,76 @@
 import time
 import sqlite3
+from io import BytesIO
+import tqdm
 import pandas as pd
 import os
 
+from PIL import Image
+from pandas import DataFrame
 
-# def sql_executor(sql_query: str, flag: str):
-#     with sqlite3.connect("db.db3") as conn:
-#         cursor = conn.cursor()
-#         cursor.execute(sql_query)
-#         match flag:
-#             case "select"
+def sql_executor(sql_query: str):
+    with sqlite3.connect('db.db3') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        return cursor.execute(sql_query)
+def create_correct_name(name: str) -> str:
+    # Формирование корректного имени для поиска в директории с картинками, обработка спецсимволов
+    return name.split('\n')[0].replace('/', "").replace(".", "").replace('-',"").replace("%", '').replace(",", '').replace("+", "").replace("'", "").replace("&", "").replace("(", "").replace(")", "")
+def find_image_product(name: str) -> str:
+    # Проверяем, существует ли файл с изображением
+    first = "Shops/All/img/" + name.split('\n')[0].replace('/', "").replace(
+            ".", "").replace(
+        '-',"").replace("%", '').replace(
+        ",", '').replace("+", "").replace(
+        "'", "").replace("&", "").replace(
+        "(", "").replace(")", "")+".png"
+    second = "Shops/All/img/"+name.replace("/", '-').replace("/", "-")+".png"
+    if os.path.exists(first):
+        return first
+    elif os.path.exists(second):
+        return second
+    else:
+        # print("################# ERROR ##############", name, first, second)
+        return "error"
+
+# Создание основной рабочей таблицы
+def create_unique_table(database: str, csv_directory: str, img_directoty: str):
+    dict_of_data: dict = {
+        "name": [],
+        "price": [],
+        "pic_url": [],
+        "address": [],
+        "name_shop": []
+    }
+
+    main_df: DataFrame = pd.concat([pd.read_csv(
+        f'{csv_directory}{csv}',
+        usecols=['name', 'price', 'pic_url', 'address', 'name_shop'])
+        for csv in [f for f in os.listdir(csv_directory) if f.endswith('.csv')]], ignore_index=True)
+    main_df.dropna(inplace=True)
+
+    # print(len(set(main_df["name"].tolist())))
+    for name in tqdm.tqdm(list(set(main_df["name"].tolist())), desc="Processing...", ncols=200):
+        # получение цены товара из DF со всеми товарами
+        price_value = main_df.loc[main_df["name"] == name, "price"].iloc[0]
+
+        # Получение адресов магазинов, в которых текущий товар в наличии
+        address_values = "|".join(set(main_df.loc[main_df["name"] == name, "address"].tolist()))
+
+        # Заполняем данные для нового DataFrame
+        dict_of_data["name"].append(name)
+        dict_of_data["price"].append(price_value)
+        # Заполнение БД картинками в бинарном виде
+        dict_of_data["pic_url"].append(find_image_product(name))
+        dict_of_data["address"].append(address_values)
+        dict_of_data["name_shop"].append(main_df.loc[main_df["name"] == name, "name_shop"].iloc[0])
 
 
-
-def from_csv_to_database(db_file: str, csv_folder: str, shop_flag: str):
-    with sqlite3.connect(db_file) as conn:
-        # Получение списка CSV-файлов в папке
-        csv_files = [f for f in os.listdir(csv_folder) if f.endswith('.csv')]
-
-        # Цикл для обработки каждого CSV-файла
-        for csv_file in csv_files:
-            # Имя таблицы будет таким же, как имя файла без расширения .csv
-            match shop_flag:
-                case "kb":
-                    table_name = "kb_"+os.path.splitext(csv_file)[0]
-                case "bristol":
-                    table_name = "bristol_" + os.path.splitext(csv_file)[0]
-
-            # Чтение CSV-файла в Pandas DataFrame
-            df = pd.read_csv(os.path.join(csv_folder, csv_file))
-            # Сохранение DataFrame в базе данных SQLite
-            df.to_sql(table_name, conn, if_exists='replace', index=False)
-
-
-def get_names_tables(flag: str = 'all'):
-    with sqlite3.connect("db.db3") as conn:
-        cursor = conn.cursor()
-        match flag:
-            case "all":
-                    return [i[0] for i in cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table' AND name != 'unique_products'").fetchall()]
-            case "kb":
-                    return [i[0] for i in cursor.execute(
-                        "SELECT name FROM sqlite_master WHERE type = 'table' AND name LIKE 'kb_%'").fetchall()]
-            case "bristol":
-                    return [i[0] for i in cursor.execute(
-                        "SELECT name FROM sqlite_master WHERE type = 'table' AND name name LIKE 'bristol_%'").fetchall()]
-
+    # Формирование финального DF для помещения в БД
+    with sqlite3.connect(database) as conn:
+        DataFrame(dict_of_data).to_sql("unique_products", conn, if_exists='replace', index=False,
+                        dtype={'name': 'TEXT', 'address': 'TEXT', 'name_shop': 'TEXT', 'price': 'TEXT',
+                               'pic_url': 'TEXT'})
+        conn.cursor().execute('DELETE from unique_products WHERE pic_url LIKE "%error%"')
 def drop_all_tables(database_file):
     try:
         with sqlite3.connect(database_file) as conn:
@@ -65,90 +90,13 @@ def drop_all_tables(database_file):
     except sqlite3.Error as e:
         print(f"Ошибка при удалении таблиц: {e}")
 
-def get_name_products():
-    with sqlite3.connect('db.db3') as conn:
-        return [i[0] for i in conn.cursor().execute("SELECT name FROM unique_products").fetchall()]
-
-def generate_sql(flag: str, name_product: str):
-    sql: str = ""
-    match flag:
-        case "for_search":
-            temp: int = 1
-            tables = get_names_tables("all")
-
-            for table in tables:
-                if temp < len(tables):
-                    try:
-                        sql += f"SELECT address FROM `{table}` WHERE name = '{name_product}' UNION "
-                    except sqlite3.OperationalError:
-                        sql += f'SELECT address FROM `{table}` WHERE name = "{name_product}" UNION '
-                    finally:
-                        temp += 1
-                else:
-                    try:
-                        sql += f"SELECT address FROM `{table}` WHERE name = '{name_product}'"
-                    except sqlite3.OperationalError:
-                        sql += f'SELECT address FROM `{table}` WHERE name = "{name_product}"'
-                    finally:
-                        temp += 1
-    return sql
-
-def create_dict_product_address():
-    with sqlite3.connect('db.db3') as conn:
-        cursor = conn.cursor()
-        dict_of_products_query: dict = {}
-        products: list = get_name_products()
-        for product in products:
-            dict_of_products_query[product] = generate_sql('for_search', product)
-            print(f"{product} -- {dict_of_products_query[product]}")
-
-        for product in dict_of_products_query:
-            try:
-                dict_of_products_query[product] = [i[0] for i in cursor.execute(dict_of_products_query[product]).fetchall()]
-            except sqlite3.OperationalError:
-                print(f"Error, {product}")
-        print(len(dict_of_products_query))
-        for i in dict_of_products_query:
-            print(f"{i} --- {dict_of_products_query[i]}")
-            break
-
-
-def create_unique_table(db_file: str):
-    # Подключение к базе данных SQLite
-    with sqlite3.connect(db_file) as conn:
-        cursor = conn.cursor()
-
-        # Создаем новую таблицу для хранения уникальных продуктов
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS unique_products (
-                id INTEGER PRIMARY KEY,
-                pic_url BLOB,
-                name_shop TEXT,
-                name TEXT NOT NULL,
-                UNIQUE (name)
-            )
-        ''')
-
-        # Получаем список всех таблиц в базе данных
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = get_names_tables('all')
-
-        # Цикл для обработки каждой таблицы
-        for table in tables:
-            print(table)
-        if "kb_" in table:
-            cursor.execute(
-                f"INSERT OR IGNORE INTO unique_products (pic_url, name_shop, name) SELECT DISTINCT id, pic_url, name_shop, name FROM `{table}`")
-        # else:
-        #     cursor.execute(
-        #         f"INSERT OR IGNORE INTO unique_products (pic_url-src, pic_url_slide-src,, name) SELECT DISTINCT id, pic_url, name_shop, name FROM `{table}`")
-
-
-
-
-
 
 if __name__ == "__main__":
     start = time.time()
-    create_dict_product_address()
-    print(f"running time - {(time.time() - start) / 60} min")
+    create_unique_table("db.db3", "Shops/All/", "Shops/All/img/")
+    # t: str = input("Введите название продукта: ")
+    # # with open ('result.txt', 'w') as write_file:
+    # #     write_file.write(f"{search_products(t, 'db.db3')}")
+    # print(search_products(t, 'db.db3'))
+    print(f"running time - {(time.time() - start) / 60 } min")
+    ##### Доделать бристоль и сделать вменяемую систему предоставления результатов поиска, добавить исполнения удаления продуктов без картинки

2d748853f256dba4de4bf477b2e99f67e226f4ef
Valery <ValerOkun@yandex.ru>
2023-12-03 14:37:34 +0300

    Реализован регистронезависимый поиск по БД

--- backend/find_products.py
+++ backend/find_products.py
@@ -1,35 +1,19 @@
-import os.path
+
 import sqlite3
 
-
-def get_name_products():
-    with sqlite3.connect('db.db3') as conn:
-        return [i[0] for i in conn.cursor().execute("SELECT name FROM unique_products").fetchall()]
-
-def search_products(search_query, database_file='db.db3'):
-    try:
-        with sqlite3.connect(database_file) as conn:
-            cursor = conn.cursor()
-
-            # Разбиваем входной запрос на отдельные слова
-            search_terms = search_query.split()
-
-            # Создаем список для хранения результатов поиска
-            results = []
-
-            # Создаем SQL-запрос с условиями LIKE для каждого слова в запросе
-            sql_query = "SELECT * FROM unique_products WHERE "
-            sql_conditions = []
-            for term in search_terms:
-                sql_conditions.append("name LIKE ?")
-            sql_query += " AND ".join(sql_conditions)
-
-            # Выполняем SQL-запрос
-            cursor.execute(sql_query, ['%' + term + '%' for term in search_terms])
-            term_results = cursor.fetchall()
-            results.extend(term_results)
-
-        # Убираем дубликаты и сортируем результаты
+def search_fts_table(search_term):
+    with sqlite3.connect("db.db3") as conn:
+        table_name = 'unique_products_fts'
+        original_table_name = 'unique_products'
+        cursor = conn.cursor()
+        cursor.execute('DROP TABLE unique_products_fts')
+        cursor.execute(f'''
+                    CREATE VIRTUAL TABLE IF NOT EXISTS {table_name} USING fts4(name, price, pic_url, address, name_shop, count, sale, min, max, tokenize=unicode61)''')
+        cursor.execute(f'''
+                INSERT INTO {table_name} SELECT * FROM {original_table_name}
+            ''')
+        # Ищем виртуальную таблицу по столбцу name, игнорируя регистр
+        results = cursor.execute(f"SELECT * FROM {table_name} WHERE name MATCH ? COLLATE NOCASE", (search_term,)).fetchall()
         unique_results = list(set(results))
         unique_results.sort()
 
@@ -42,13 +26,11 @@ def search_products(search_query, database_file='db.db3'):
                     'sale': [float(i) for i in unique_results[i][6].split(",")],
                     'min': unique_results[i][7],
                     'max': unique_results[i][8]
-                    } for i in range (len(unique_results))}
-    except sqlite3.Error as e:
-        print(f"Ошибка при выполнении поиска: {e}")
-        return None
+                    } for i in range(len(unique_results))}
 
-res = search_products(input("Введите название продукта: "))
+
+
+res = search_fts_table(input("Введите название продукта: "))
 for i in res:
     print(f"---- {i} ----  \n {res[i]['name']}\n{res[i]['address']}\n{res[i]['pic_url']}\n{res[i]['price']}\n{res[i]['name_shop']}\n"
           f"{res[i]['count']}\n{res[i]['sale']}\n{res[i]['min']}\n{res[i]['max']}")
-    print(os.path.exists(f'{res[i]["pic_url"]}'))
---
+++ backend/find_products_old.py
@@ -0,0 +1,54 @@
+import os.path
+import sqlite3
+
+
+def get_name_products():
+    with sqlite3.connect('db.db3') as conn:
+        return [i[0] for i in conn.cursor().execute("SELECT name FROM unique_products").fetchall()]
+
+def search_products(search_query, database_file='db.db3'):
+    try:
+        with sqlite3.connect(database_file) as conn:
+            cursor = conn.cursor()
+
+            # Разбиваем входной запрос на отдельные слова
+            search_terms = search_query.split()
+
+            # Создаем список для хранения результатов поиска
+            results = []
+
+            # Создаем SQL-запрос с условиями LIKE для каждого слова в запросе
+            sql_query = "SELECT * FROM unique_products WHERE "
+            sql_conditions = []
+            for term in search_terms:
+                sql_conditions.append("name LIKE ? COLLATE NOCASE")
+            sql_query += " AND ".join(sql_conditions)
+
+            # Выполняем SQL-запрос
+            cursor.execute(sql_query, ['%' + term + '%' for term in search_terms])
+            term_results = cursor.fetchall()
+            results.extend(term_results)
+
+        # Убираем дубликаты и сортируем результаты
+        unique_results = list(set(results))
+        unique_results.sort()
+
+        return {i: {'name': unique_results[i][0],
+                    'price': unique_results[i][1],
+                    'pic_url': unique_results[i][2],
+                    'address': unique_results[i][3].split("|"),
+                    'name_shop': unique_results[i][4],
+                    'count': [int(i) for i in unique_results[i][5].split(",")],
+                    'sale': [float(i) for i in unique_results[i][6].split(",")],
+                    'min': unique_results[i][7],
+                    'max': unique_results[i][8]
+                    } for i in range (len(unique_results))}
+    except sqlite3.Error as e:
+        print(f"Ошибка при выполнении поиска: {e}")
+        return None
+
+res = search_products(input("Введите название продукта: "))
+for i in res:
+    print(f"---- {i} ----  \n {res[i]['name']}\n{res[i]['address']}\n{res[i]['pic_url']}\n{res[i]['price']}\n{res[i]['name_shop']}\n"
+          f"{res[i]['count']}\n{res[i]['sale']}\n{res[i]['min']}\n{res[i]['max']}")
+    print(os.path.exists(f'{res[i]["pic_url"]}'))
